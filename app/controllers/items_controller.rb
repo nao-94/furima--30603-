f5e8ca40462f47773514f8f861f18094c4220bbc
nao-94 <naot9423@gmail.com>
2020-10-15 16:39:00 +0900

    first-commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,14 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
+    @item = Item.find(params[:item_id])
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+  end
+
 end
